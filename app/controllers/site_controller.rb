239d98efc26dd425254a6bf583b1063b0aa347a1
Jeremy Woertink <jeremywoertink@gmail.com>
2014-07-10 10:00:49 -0600

    added about page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,11 @@
 class SiteController < ApplicationController
 
   def index
-    
+
+  end
+
+  def about
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'site#index'
+  get '/about' => 'site#about'
 end
