65cac0c084003ab032a3d3a64393802120090bd9
Jeremy Woertink <jeremywoertink@gmail.com>
2014-07-11 11:50:44 -0600

    added a map into the location page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,20 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require gmaps
 //= require_tree .
+
+$(function() {
+  loadMap();
+});
+
+var loadMap = function() {
+  $map = $('#map');
+  if($map.length) {
+    var gmap = new GMaps({
+      div: '#map',
+      lat: $map.data('lat'),
+      lng: $map.data('lon')
+    });
+  }
+};
